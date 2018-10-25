27b04c58aeba743c47df776f46967492a0a5e752
intelsy <intel.sy.408@gmail.com>
2018-10-25 09:58:44 +0900

    Edit some constants

--- final_problem.js
+++ final_problem.js
@@ -1,21 +1,29 @@
-const data = [
-  	{id: 1, first:'Steve', family:'Wozniak', favFood:'Apple', BirthDay:0812},
-  	{id: 2, first:'Steve', family:'Jobs', favFood:'Apple', BirthDay:0913},
-  	{id: 3, first:'Mark', family:'Zukerberg', favFood:'Banana', BirthDay:0322},
-  	{id: 4, first:'Elon', family:'Musk', favFood:'Toast', BirthDay:1121},
+const personData = [
+  	{id: 1, firstName: 'Steve', familyName: 'Wozniak', favFood: 'Apple', BirthDay: 0812},
+  	{id: 2, firstName: 'Steve', familyName: 'Jobs', favFood: 'Apple', BirthDay: 0913},
+  	{id: 3, firstName: 'Mark', familyName: 'Zukerberg', favFood: 'Banana', BirthDay: 0322},
+  	{id: 4, firstName: 'Elon', familyName: 'Musk', favFood: 'Toast', BirthDay: 1121},
 ];
 
 //// 1:map
-const people = data.map((person) => {
-  return {id: person.id, name:person.first+person.family, favFood:person.favFood}
+const mappedPersonData = personData.map((person) => {
+  return {
+    id: person.id,
+    fullName: person.firstName + person.familyName,
+    favFood: person.favFood
+  }
 });
-console.log(data);
-console.log(people);
+console.log('[mappedPersonData]')
+console.log(mappedPersonData);
 
 //// 2:find
-const id2Person = people.find((person) => {return person.id == 2});
-console.log(id2Person);
+const id2Person = mappedPersonData.find((person) => {return person.id == 2});
+console.log('[person who has id 2]')
+console.log(id2Person.fullName);
 
 //// 3:filter
-const applePerson = people.filter((person) => {return person.favFood == 'Apple'});
-console.log(applePerson);
+const applePerson = mappedPersonData.filter((person) => {return person.favFood == 'Apple'});
+console.log('[people who like Apple]')
+applePerson.forEach((person) => {
+    console.log(person.fullName);
+})
